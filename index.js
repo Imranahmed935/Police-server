acb445f6851d78a71d626cc8b0a72684456eb98d
Imranahmed935 <imrantahir9918@gmail.com>
2025-03-29 22:33:29 +0600

    experience updated form created

--- index.js
+++ index.js
@@ -11,7 +11,9 @@ dotenv.config();
 
 
 const uri = `mongodb+srv://${process.env.Police_DB}:${process.env.Police_Pass}@cluster0.haqk7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
-console.log(uri)
+
+
+
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
@@ -22,7 +24,77 @@ const client = new MongoClient(uri, {
 });
 
 async function run() {
+    const usersInfo = client.db('PoliceData').collection('users');
   try {
+
+    //create user
+    app.post('/users', async (req,res)=>{
+        const data = req.body;
+        const result = await usersInfo.insertOne(data);
+        res.send(result)
+    })
+
+    //update user by Email
+    app.put('/users/:email', async (req, res) => {
+      const { image, aboutData, experienceData } = req.body;
+      const email = req.params.email;
+      
+      const updateDoc = { $set: {} };
+      
+      // For single fields
+      if (image) updateDoc.$set.profilePic = image;
+      if (aboutData) updateDoc.$set.aboutInfo = aboutData;
+      
+      // For experience data (array)
+      if (experienceData) {
+        updateDoc.$push = { 
+          experienceData: {
+            $each: [experienceData],
+            $position: 0  // Adds at beginning of array
+          }
+        };
+      }
+      
+      try {
+        const result = await usersInfo.updateOne(
+          { email },
+          updateDoc,
+          { upsert: true }
+        );
+        
+        res.status(200).json({
+          success: true,
+          updatedFields: Object.keys(updateDoc.$set || {}).concat(
+            Object.keys(updateDoc.$push || {})
+          )
+        });
+        
+      } catch (error) {
+        console.error('Update error:', error);
+        res.status(500).json({ 
+          success: false,
+          error: 'Update failed',
+          details: error.message 
+        });
+      }
+    });
+    
+    // get a single user by email
+    app.get('/users/:email', async(req, res)=>{
+      const email = req.params.email;
+      const result = await usersInfo.findOne({email});
+      res.send(result)
+    })
+
+    //get all users for info
+    // app.get('/users/update/:email', async(req, res)=>{
+    //   const email = req.params.email;
+    //   const result = await usersInfo.findOne({email});
+    //   res.send(result)
+    // })
+
+
+
     // Connect the client to the server	(optional starting in v4.7)
     // await client.connect();
     // Send a ping to confirm a successful connection
